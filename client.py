44879bdc09c0faf5bdd19ce7d61450a2bf721fca
Antype Cryptous <3c7975@gmail.com>
2026-08-27 21:55:05 +1000

    updated voice and functions

--- client.py
+++ client.py
@@ -5,22 +5,23 @@ from threading import Thread
 from PIL import Image, ImageTk
 # from tkinterdnd2 import DND_FILES, TkinterDnD
 import imageio
-import os, queue
-import pyttsx3
+import os, queue, time
+import edge_tts
+import pyaudio
+from io import BytesIO
+from pydub import AudioSegment
 import ast
 import json
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
-img_path = os.path.join(script_dir, "./img/führer/default.jpg")
+img_path = os.path.join(script_dir, "./img/führer/grok-71d4db52-e556-4e72-8998-f1112a05fc40.jpg")
 # server_path = os.path.join(script_dir, "./server.py")
-video = imageio.get_reader("./img/führer/speech.mp4")
+video = imageio.get_reader("./img/führer/grok-video-bba23814-090d-4ad4-9309-b66c4da8f74a.mp4")
 
 store_frame = queue.Queue(maxsize=10) # get 10 frames at most, pause update_frame() if full
 
 host = '127.0.0.1'
 port = 5000
-
-tts_engine = pyttsx3.init()
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
@@ -28,8 +29,39 @@ try:
 except ConnectionRefusedError:
     print("No server launched. Try again.")
 
-image_generator = video.iter_data()
-count = 0
+# tts_engine = pyttsx3.init()
+# voices = tts_engine.getProperty("voices")
+
+# def speaking(string):
+#     tts_engine.say(string)
+#     tts_engine.runAndWait()
+
+# def speaking(TEXT) -> None:
+#     communicator = edge_tts.Communicate(TEXT, VOICE, boundary="WordBoundary")
+#     audio_chunks = []
+
+#     pyaudio_instance = pyaudio.PyAudio()
+#     audio_stream = pyaudio_instance.open(format=pyaudio.paInt16, channels=1, rate=24000, output=True)
+
+#     for chunk in communicator.stream_sync():
+#         if chunk["type"] == "audio" and chunk["data"]:
+#             audio_chunks.append(chunk["data"])
+#             if len(audio_chunks) >= CHUNK_SIZE:
+#                 play_audio_chunks(audio_chunks, audio_stream)
+#                 audio_chunks.clear()
+
+#     # Play the rest of the audio
+#     play_audio_chunks(audio_chunks, audio_stream)
+
+#     audio_stream.stop_stream()
+#     audio_stream.close()
+#     pyaudio_instance.terminate()
+
+# def play_audio_chunks(chunks: list[bytes], stream: pyaudio.Stream) -> None:
+#     stream.write(AudioSegment.from_mp3(BytesIO(b''.join(chunks))).raw_data) 
+
+# image_generator = video.iter_data()
+# count = 0
 # speech_length_ticks = 0
 
 # if you want to manually adjust the speed of the video playback
@@ -46,47 +78,130 @@ count = 0
 #             store_frame.put(image)
 #             count+=1
 
-def speaking(string):
-    tts_engine.say(string)
-    tts_engine.runAndWait()
 
-def responding(widget, index, string):
-    global image_generator, count
-    widget.configure(state="normal")
-    if len(string) > 0:
-        widget.insert(index, string[0])
-        try:
-            image = next(image_generator)
-            store_frame.put(image)
-        except StopIteration:
-            print(f"finished iterating frames from video: {count+1} times")
-            image_generator = video.iter_data()
-            image = next(image_generator)
-            store_frame.put(image)
-            count+=1
-        if len(string) > 1:
+VOICE="ja-JP-NanamiNeural"
+CHUNK_SIZE = 20
+
+
+
+def speaking(TEXT) -> None:
+    global audio_chunks, audio_stream, pyaudio_instance, word_list
+    communicator = edge_tts.Communicate(TEXT, VOICE, boundary="WordBoundary")
+    audio_chunks = []
+    word_boundary = []
+    word_list = TEXT.split()
+    offset_list = []
+    duration_list = []
+
+    pyaudio_instance = pyaudio.PyAudio()
+    audio_stream = pyaudio_instance.open(format=pyaudio.paInt16, channels=1, rate=24000, output=True)
+
+    for chunk in communicator.stream_sync():
+        # if chunk["type"] == "audio":
+            # print("audio chunk")
+            # print(chunk)
+        if chunk["type"] == "WordBoundary":
+            # word_list.append(chunk["text"])
+            offset_list.append(chunk["offset"])
+            duration_list.append(chunk["duration"])
+            word_boundary.append(chunk)
+        if chunk["type"] == "audio" and chunk["data"]:
+            audio_chunks.append(chunk["data"])
+            # if len(audio_chunks) >= CHUNK_SIZE:
+                # play_audio_chunks(audio_chunks, audio_stream)
+                # audio_chunks.clear()
+    # Play the rest of the audio
+    # print(word_boundary)
+    # print(word_list)
+    # print(offset_list)
+    # print(duration_list)
+    chat_container.configure(state="normal")
+    chat_container.insert(END, "Führer: ", "name")
+    chat_container.configure(state="disabled")
+    # type_thread = Thread(target=responding, args=(chat_container, "end", word_list, offset_list, duration_list), daemon=False)
+    # type_thread.start()
+    anime(chat_container, "end", word_list, offset_list, duration_list)
+    play_audio_chunks(audio_chunks, audio_stream)
+    audio_chunks.clear()
+    audio_stream.stop_stream()
+    audio_stream.close()
+    pyaudio_instance.terminate()
+
+def play_audio_chunks(chunks: list[bytes], stream: pyaudio.Stream) -> None:
+    stream.write(AudioSegment.from_mp3(BytesIO(b''.join(chunks))).raw_data) 
+
+
+image_generator = video.iter_data()
+count = 0
+
+word_num = 0
+start_time = 0
+current_word = ""
+checked_time = False
+is_fuhrer_typing = False
+
+def anime(widget, index, word_list, offset_list, duration_list):
+    global image_generator, count, word_num, start_time, current_word, checked_time, is_fuhrer_typing
+    is_fuhrer_typing = True
+    if checked_time == False:
+            start_time = time.perf_counter_ns()
+            checked_time = True
+    TEXT = word_list[word_num]
+    current_offset = offset_list[word_num]/10_000_000 # edge tts uses 100 nanoseconds
+    time_diff = (time.perf_counter_ns() - start_time)/1_000_000_000 # time.perf_counter_ns() uses nanoseconds
+    if time_diff < current_offset:
+            widget.after(int((current_offset-time_diff)*1000), anime, widget, index, word_list, offset_list, duration_list)
+    else:
+        widget.configure(state="normal")
+        if len(TEXT) > 0:
+            widget.insert(index, TEXT[0])
+            current_word += TEXT[0]
+            delay = (duration_list[word_num]/len(TEXT))/10_000_000
+            try:
+                image = next(image_generator)
+                store_frame.put(image)
+            except StopIteration:
+                print(f"finished iterating frames from video: {count+1} times")
+                image_generator = video.iter_data()
+                image = next(image_generator)
+                store_frame.put(image)
+                count+=1
+            # if len(" ".join(word_list))-(len(word_list)-1) > 1:
             index = widget.index(f"{index} + 1 char")
-            widget.after(50, responding, widget, index, string[1:])
-    widget.configure(state="disabled")
-    chat_container.see("end")
+            word_list[word_num] = TEXT[1:]
+            widget.after(int(delay*1000), anime, widget, index, word_list, offset_list, duration_list)
+        else:
+            if word_num != len(word_list)-1:
+                widget.insert(index, " ")
+                word_num +=1
+                current_word = ""
+                widget.after(0, anime, widget, index, word_list, offset_list, duration_list)
+            else:
+                print("hello, this is the end of the statement")
+                word_num = 0
+                start_time = 0
+                current_word = ""
+                checked_time = False
+                is_fuhrer_typing = False
+                entry.configure(state="normal")
+                entry.delete(0, END)
+                chat_container.configure(state="disabled")
+                chat_container.see("end")
 
 def receive_message():
-    global speech_length_ticks
     while True:
         raw = client_socket.recv(1024)
         if not raw:
             print("")
-        data = raw.decode('utf-8')
-        print(data)
-        # speech_length_ticks = len(data)*1.1
-        # sub_thread = Thread(target=update_frame, daemon=True)
-        # sub_thread.start()
-        chat_container.configure(state="normal")
-        chat_container.insert(END, "Führer: ", "name")
-        chat_container.configure(state="disabled")
+        else:
+            data = raw.decode('utf-8')
+            print(data)
+            # speech_length_ticks = len(data)*1.1
+            # sub_thread = Thread(target=update_frame, daemon=True)
+            # sub_thread.start()
 
-        responding(chat_container, "end", data+"\n")
-        speaking(data)
+            # responding(chat_container, "end", data+"\n")
+            speaking(data)
 
 def submit(user_entry):
     message = user_entry.get()
@@ -94,12 +209,17 @@ def submit(user_entry):
     entry.delete(0, END)
     client_socket.sendall(message.encode())
     chat_container.configure(state="normal")
+    if chat_container.get("1.0", "end-1c"):
+        chat_container.insert(END, "\n")
     chat_container.insert(END, "Goy: ", "name")
     chat_container.insert(END, message+"\n")
     chat_container.configure(state="disabled")
     chat_container.see("end")
 
 def update_gui():
+    if is_fuhrer_typing==True:
+        entry.insert(0, "Führer is responding...")
+        entry.configure(state="disabled")
     if store_frame.empty():
         insert_image.after(33, update_gui)
     else:
@@ -173,7 +293,7 @@ def build_gui():
     name.pack(side="top")
 
     img = Image.open(img_path)
-    # img.thumbnail((768, 1168))
+    img.thumbnail((768, 1168))
     tk_image = ImageTk.PhotoImage(img)
     insert_image = Label(top_frame, image=tk_image)
     insert_image.image = tk_image
--- test.py
+++ test.py
@@ -6,29 +6,161 @@
 # for i, (gs, ps, audio) in enumerate(generator):
 #     sf.write(f'{i}.wav', audio, 24000) # Saves high-quality 24kHz audio
 
-from tkinter import filedialog
-from tkinter import Tk
-import json
-import ast
+import edge_tts
+import pyaudio
+from io import BytesIO
+from pydub import AudioSegment
+import time, os
+from threading import Thread
 
-Tk().withdraw()
-def load_history():
-    chat_history = filedialog.askopenfile()
-    print(chat_history)
-    read_file = chat_history.read()
-    read_file = ast.literal_eval(read_file)
-    # client_socket.sendall(read_file.encode())
-    # chat_container.configure(state="normal")
-    print(type(read_file))
-    for i in read_file:
-        print(i)
-        print(i["role"])
-        # if i["role"] == "user":
-        #     chat_container.insert(END, "Goy: ", "name")
-        # elif i["role"] == "assistant":
-        #     chat_container.insert(END, "Führer: ", "name")
-        # chat_container.insert(END, i["content"]+"\n")
-load_history()
+VOICE="ja-JP-NanamiNeural"
+CHUNK_SIZE = 20
+
+def typeit(word_list, offset_list, duration_list):
+    offset_list.insert(0, 0)
+    duration_list.insert(0, 0)
+    for index, word in enumerate(word_list):
+        current_duration = duration_list[index+1]/10_000_000
+        total_offset = offset_list[index+1]/10_000_000
+        last_duration = duration_list[index]/10_000_000
+        last_offset = offset_list[index]/10_000_000
+        offset = (total_offset - last_offset) - last_duration
+        delay = current_duration/len(word)
+        print("delay", delay)
+        time.sleep(offset)
+        for i in word:
+            print(i, end="", flush=True)
+            time.sleep(delay)
+        print(" ", end="")
+
+    print("\n")
+
+# a = [{'type': 'WordBoundary', 'offset': 1250000, 'duration': 4500000, 'text': 'hello'}, {'type': 'WordBoundary', 'offset': 7625000, 'duration': 2375000, 'text': 'how'}, {'type': 'WordBoundary', 'offset': 10125000, 'duration': 2375000, 'text': 'are'}, {'type': 'WordBoundary', 'offset': 12625000, 'duration': 2875000, 'text': 'you'}, {'type': 'WordBoundary', 'offset': 15625000, 'duration': 6500000, 'text': 'doing'}]
+# word_list = ['hello', 'how', 'are', 'you', 'doing']
+# offset_list = [1250000, 7625000, 10125000, 12625000, 15625000]
+# duration_list = [4500000, 2375000, 2375000, 2875000, 6500000]
+
+char_num = 0
+word_num = 0
+start_time = 0
+current_word = ""
+def typeit(word_list, offset_list, duration_list):
+    global char_num, word_num, start_time, current_word
+    if word_num==0 and char_num==0:
+        start_time = time.time_ns()
+    word = word_list[word_num]
+    delay = (duration_list[word_num]/len(word))/10_000_000
+    current_offset = offset_list[word_num]
+    while (time.time_ns() - start_time) < current_offset:
+        continue
+    else:
+        if len(word) > 0 and word != current_word:
+            char = word[char_num]
+            print(char, end="", flush=True)
+            current_word += char
+            char_num +=1
+            time.sleep(delay)
+            typeit(word_list, offset_list, duration_list)
+        else:
+            if word_num != len(word_list)-1:
+                word_num +=1
+                char_num = 0
+                current_word = ""
+                print(" ", end="")
+                typeit(word_list, offset_list, duration_list)
+
+
+def speaking(TEXT) -> None:
+    global audio_chunks, audio_stream, pyaudio_instance, word_list
+    communicator = edge_tts.Communicate(TEXT, VOICE, boundary="WordBoundary")
+    audio_chunks = []
+    word_boundary = []
+    word_list = []
+    offset_list = []
+    duration_list = []
+
+    pyaudio_instance = pyaudio.PyAudio()
+    audio_stream = pyaudio_instance.open(format=pyaudio.paInt16, channels=1, rate=24000, output=True)
+
+    for chunk in communicator.stream_sync():
+        # if chunk["type"] == "audio":
+            # print("audio chunk")
+            # print(chunk)
+        if chunk["type"] == "WordBoundary":
+            word_list.append(chunk["text"])
+            offset_list.append(chunk["offset"])
+            duration_list.append(chunk["duration"])
+            word_boundary.append(chunk)
+        if chunk["type"] == "audio" and chunk["data"]:
+            audio_chunks.append(chunk["data"])
+            # if len(audio_chunks) >= CHUNK_SIZE:
+                # play_audio_chunks(audio_chunks, audio_stream)
+                # audio_chunks.clear()
+    # Play the rest of the audio
+    print(word_boundary)
+    print(word_list)
+    print(offset_list)
+    print(duration_list)
+    type_thread = Thread(target=typeit, args=(word_list,offset_list,duration_list), daemon=False)
+    type_thread.start()
+    play_audio_chunks(audio_chunks, audio_stream)
+    audio_chunks.clear()
+    audio_stream.stop_stream()
+    audio_stream.close()
+    pyaudio_instance.terminate()
+
+def play_audio_chunks(chunks: list[bytes], stream: pyaudio.Stream) -> None:
+    stream.write(AudioSegment.from_mp3(BytesIO(b''.join(chunks))).raw_data) 
+
+
+TEXT = input(" >> ")
+speaking(TEXT)
+
+
+# def speaking(TEXT):
+#     communicator = edge_tts.Communicate(TEXT, VOICE, boundary="WordBoundary")
+
+#     for chunk in communicator.stream_sync():
+
+        # if chunk["type"] == "audio":
+        #     # print("audio chunk")
+        #     print(chunk)
+
+        # elif chunk["type"] == "WordBoundary":
+        #     print(chunk)
+
+# start_time = time.time()
+# speaking(TEXT)
+# type_thread = Thread(target=typeit, args=(TEXT,), daemon=False)
+# type_thread.start()
+
+
+
+# print(f"Time taken: {time.time() - start_time}")
+
+# from tkinter import filedialog
+# from tkinter import Tk
+# import json
+# import ast
+
+# Tk().withdraw()
+# def load_history():
+#     chat_history = filedialog.askopenfile()
+#     print(chat_history)
+#     read_file = chat_history.read()
+#     read_file = ast.literal_eval(read_file)
+#     # client_socket.sendall(read_file.encode())
+#     # chat_container.configure(state="normal")
+#     print(type(read_file))
+#     for i in read_file:
+#         print(i)
+#         print(i["role"])
+#         # if i["role"] == "user":
+#         #     chat_container.insert(END, "Goy: ", "name")
+#         # elif i["role"] == "assistant":
+#         #     chat_container.insert(END, "Führer: ", "name")
+#         # chat_container.insert(END, i["content"]+"\n")
+# load_history()
 
 
 
